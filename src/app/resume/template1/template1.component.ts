5d0905a75c19124689482799417f2365cf40a91b
Aayushi <aayushi012001@gmail.com>
2022-10-16 02:47:11 +0530

    committed by Aayushi Joshua

--- src/app/resume/template1/template1.component.ts
+++ src/app/resume/template1/template1.component.ts
@@ -12,64 +12,58 @@ import { project } from 'src/app/project';
   styleUrls: ['./template1.component.css'],
 })
 export class Template1Component implements OnInit {
-   dataSource!:any;
-   proj!:project;
-   projects:project[]=[];
-   edu!:education;
-   educate:education[]=[];
-  name='';
-  title!:string
-   desc!:string
-  constructor(private store:AngularFirestore,private serv:AuthService) { }
+  dataSource!: any;
+  proj!: project;
+  projects: project[] = [];
+  edu!: education;
+  educate: education[] = [];
+  name = '';
+  title!: string;
+  desc!: string;
+  constructor(private store: AngularFirestore, private serv: AuthService) {}
 
-
-
-  pushing(data:project){
-    this.projects.push(data)
+  pushing(data: project) {
+    this.projects.push(data);
   }
-  pushing1(data:education){
-    this.educate.push(data)
+  pushing1(data: education) {
+    this.educate.push(data);
   }
-  
-  
+
   ngOnInit(): void {
     // this.getAll();
 
-    this.serv.messageSource.subscribe((message)=>{
-      this.proj={
-       title:message[0],
-        desc:message[1]
-      }
-      
+    this.serv.messageSource.subscribe((message) => {
+      this.proj = {
+        title: message[0],
+        desc: message[1],
+      };
 
-      this.pushing(this.proj)
-    
-    })
-    this.serv.msgSource.subscribe((message)=>{
-      this.edu={
-        education:message[0],
-        school:message[1],
-        city:message[2],
-        sdate:message[3],
-        edate:message[4],
-      }
-      console.log(this.edu.school)
-      this.pushing1(this.edu)
-    })
+      this.pushing(this.proj);
+    });
+    this.serv.msgSource.subscribe((message) => {
+      this.edu = {
+        education: message[0],
+        school: message[1],
+        city: message[2],
+        sdate: message[3],
+        edate: message[4],
+      };
+      console.log(this.edu.school);
+      this.pushing1(this.edu);
+    });
   }
 
-
-  getAll(){
-    this.store.collection('ResumeDetails',ref=>ref.where('Email','==',localStorage.getItem('email'))).snapshotChanges().subscribe((response=>{
-      this.dataSource=response.map(item=>
-        Object.assign({id:item.payload.doc.id},item.payload.doc.data()))
+  getAll() {
+    this.store
+      .collection('ResumeDetails', (ref) =>
+        ref.where('Email', '==', localStorage.getItem('email'))
+      )
+      .snapshotChanges()
+      .subscribe((response) => {
+        this.dataSource = response.map((item) =>
+          Object.assign({ id: item.payload.doc.id }, item.payload.doc.data())
+        );
         // console.log(this.dataSource.Email)
-    }))
-
-}
-
-
-
-
-
+      });
+  }
 }
