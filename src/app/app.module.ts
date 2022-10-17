4f2785d832017c1cd4c87f4fd0d45129c68e939d
Durvankur Balkawade <durvankurbalkawade@gmail.com>
2022-10-17 17:34:07 +0530

    commited by Durvankur

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import {CoverLetterModule} from './cover-letter/cover-letter.module'
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -47,6 +48,7 @@ import {MatTabsModule} from '@angular/material/tabs';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CoverLetterModule,
     BrowserAnimationsModule,
     MatButtonModule,
     MatMenuModule,
